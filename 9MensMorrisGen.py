3bc2c4d2ed20afd29520a4636ad480d3637497c7
Nikola Dimic <dimic.nikola@gmail.com>
2018-12-28 19:24:37 +0100

    logging fixed

--- 9MensMorrisGen.py
+++ 9MensMorrisGen.py
@@ -89,13 +89,12 @@ def normalize_population(population):
     new_pop = []
 
     for i in range(len(population)):
-        new_pop.append((population[i][0], population[i]
-                        [1] + abs(population[-1][1])+1))
+        new_pop.append((population[i][0], population[i][1] + abs(population[-1][1]) + 1))
 
     normalized_array = []
     previous = 0
     for i in range(len(new_pop)):
-        normalized_array.append(previous+new_pop[i][1])
+        normalized_array.append(previous + new_pop[i][1])
         previous = normalized_array[i]
 
     norm = float(normalized_array[-1])
@@ -175,6 +174,56 @@ def unit_to_str(unit):
     return unit_string
 
 
+def tournament_setup(population, enemies, filename="unos.txt"):
+    """
+    Writing into file - adding population and enemies that will play the tournament
+
+    :param population: population of players
+    :param enemies: enemies that the players will play against
+    :param filename: input file path for tournament
+    """
+    try:
+        f = open(filename, "w")
+        f.write(str(len(population)) + " " + str(len(enemies)) + "\n")
+        for unit in population:
+            f.write(unit_to_str(unit) + "\n")
+        for enemy in enemies:
+            f.write(unit_to_str(enemy) + "\n")
+        f.flush()
+
+    except IOError:
+        print("IO error while writing into file unos.txt")
+
+
+def tournament_results(filename="rezultat.txt"):
+    """
+    Read tournament results (fitness) from file
+
+    :param filename: path for tournament results
+    :return: array with fitness for each unit of population
+    """
+    try:
+        f = open(filename, "r")
+        lines = f.readlines()
+        results = []
+        for line in lines:
+            if line.__contains__("fitness"):
+                results.append(int(line[-3:]))
+        return results
+
+    except IOError:
+        print("IO error while reading results of matches from rezultat.txt")
+
+
+def tournament_run(program="./a.out"):
+    """
+    Run tournament
+    :param program: path to tournament executable
+    """
+    os.system(program)
+    time.sleep(0.1)
+
+
 def calculate_population_fitness(population, enemies):
     """
     Calculates the fitness of all units in the population and adds that fitness to the units
@@ -183,30 +232,14 @@ def calculate_population_fitness(population, enemies):
     :param enemies: enemies for units to play 9 men morris with
     :return: population with units that have fitness values
     """
-    try:
-        f = open("unos.txt", "w")
-        f.write(str(len(population)) + " " + str(len(enemies)) + "\n")
-        for unit in population:
-            f.write(unit_to_str(unit) + "\n")
-        for enemy in enemies:
-            f.write(unit_to_str(enemy) + "\n")
-        f.flush()
-        os.system("./a.out")
-        time.sleep(0.1)
+    tournament_setup(population, enemies)
+    tournament_run()
+    results = tournament_results()
 
-        f = open("rezultat.txt", "r")
-        lines = f.readlines()
-        results = []
-        for line in lines:
-            if line.__contains__("fitness"):
-                results.append(int(line[-3:]))
-
-        if len(results) != len(population):
-            print("Bad results")
-            exit()
-
-    except Exception:
-        print("IO error")
+    if len(results) != len(population):
+        logging.warning("Number of results different then number of units in population")
+        print("Number of results different then number of units in population")
+        exit()
 
     pop_with_fitness = []
     for i in range(len(population)):
@@ -246,11 +279,11 @@ def four_point_crossover(first_parent, second_parent):
     first_parent = first_parent[0]
     second_parent = second_parent[0]
     first_child = first_parent[:points[0]] + second_parent[points[0]:points[1]] + \
-        first_parent[points[1]:points[2]] + \
-        second_parent[points[2]:points[3]] + first_parent[points[3]:]
+                  first_parent[points[1]:points[2]] + \
+                  second_parent[points[2]:points[3]] + first_parent[points[3]:]
     second_child = second_parent[:points[0]] + first_parent[points[0]:points[1]] + \
-        second_parent[points[1]:points[2]] + \
-        first_parent[points[2]:points[3]] + second_parent[points[3]:]
+                   second_parent[points[1]:points[2]] + \
+                   first_parent[points[2]:points[3]] + second_parent[points[3]:]
 
     return [(first_child, -100), (second_child, -100)]
 
@@ -267,12 +300,12 @@ def mutation(unit):
         if mutation_param <= 0.08:
             addition_param = random.random()
             if addition_param < 0.5:
-                if check_range(i, unit_data[i]+1):
+                if check_range(i, unit_data[i] + 1):
                     unit_data[i] += 1
                 else:
                     unit_data[i] -= 1
             else:
-                if check_range(i, unit_data[i]-1):
+                if check_range(i, unit_data[i] - 1):
                     unit_data[i] -= 1
                 else:
                     unit_data[i] += 1
@@ -295,26 +328,65 @@ def remove_duplicates(population):
     return population
 
 
+def init_enemies():
+    """
+    Initialize enemies for fitness function
+    :return:
+    """
+    return [([19, 16, 3, 27, 7, 8, 4, 23, 27, 19, 17, 24, 29, 2, 9208, 18, 15, 36, 9975], -100)]
+    # ([19, 16, 4, 24, 7, 9, 6, 21, 27, 12, 34, 8, 54, 1, 7215, 21, 6, 33, 8788], -100),
+    # ([23, 15, 4, 28, 13, 10, 1, 18, 26, 8, 10, 27, 21, 1, 6662, 16, 5, 40, 5876], -100),
+    # ([15, 17, 2, 9, 27, 18, 6, 33, 11, 20, 25, 14, 48, 2, 5484, 10, 24, 20, 9864], -100)]
+
+
+def log_population(population, iteration):
+    """
+    Log population using logger
+    :param population: population to be logged
+    :param iteration: int
+    """
+    logging.info("-" * 77)
+    logging.info("Iteration no. " + str(iteration))
+    logging.info("-" * 77)
+    for el in sort_population(population):
+        logging.info(str(el[1]) + " " + str(el[0]))
+
+    logging.info("-" * 77)
+    logging.info("Best unit: " + unit_to_str(population[0]))
+    logging.info("Fitness: " + str(population[0][1]))
+    logging.info("Worst unit: " + unit_to_str(population[-1]))
+    logging.info("Fitness: " + str(population[-1][1]))
+
+
+def log_crossover_and_mutation(parents, children):
+    """
+    Log parents -> children transition after crossover and mutation
+    :param parents: parent units
+    :param children: children units
+    """
+    logging.info("-" * 50)
+    logging.info(str(parents[0][0]) + " +")
+    logging.info(str(parents[1][0]))
+    logging.info("-" * 50)
+    logging.info(str(children[0][0]))
+    logging.info(str(children[1][0]))
+
+
 def main():
-    enemies = [([19, 16, 3, 27, 7, 8, 4, 23, 27, 19, 17, 24, 29, 2, 9208, 18, 15, 36, 9975], -100),
-               ([19, 16, 4, 24, 7, 9, 6, 21, 27, 12, 34,
-                 8, 54, 1, 7215, 21, 6, 33, 8788], -100),
-               ([23, 15, 4, 28, 13, 10, 1, 18, 26, 8, 10,
-                 27, 21, 1, 6662, 16, 5, 40, 5876], -100),
-               ([15, 17, 2, 9, 27, 18, 6, 33, 11, 20, 25, 14, 48, 2, 5484, 10, 24, 20, 9864], -100)]
     logging.basicConfig(filename='information.log',
                         format='%(levelname)s: %(asctime)s -- %(message)s',
                         level=logging.INFO, datefmt='%d/%m %I:%M %p')
 
+    num_of_iterations = 4
+    enemies = init_enemies()
     population = initial_population()
     pop = calculate_population_fitness(population, enemies)
     iteration = 0
-    for el in sort_population(pop):
-        logging.info(str(el[1]) + " " + str(el[0]))
+    log_population(pop, iteration)
 
-    while iteration < 4:
+    while iteration < num_of_iterations:
         selected = selection(pop)
-        for i in range(len(selected)/2):
+        for i in range(len(selected) / 2):
             unit1 = random.choice(selected)
             selected.remove(unit1)
             unit2 = random.choice(selected)
@@ -324,6 +396,7 @@ def main():
             child1 = mutation(child1)
             child2 = mutation(child2)
 
+            log_crossover_and_mutation([unit1, unit2], [child1, child2])
             pop.append(child1)
             pop.append(child2)
 
@@ -344,10 +417,9 @@ def main():
         pop = new_generation + old_generation
         pop = elite_selection(sort_population(pop), 100)
         iteration += 1
+        log_population(pop, iteration)
 
-        logging.info("------------------------------------------------")
-        for el in sort_population(pop):
-            logging.info(str(el[1]) + " " + str(el[0]))
+    logging.info("THE END")
 
     return 0
 
